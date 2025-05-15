72c06bfd284f7b40fb2b66fc72b8e82cbcfeff83
Aman Kumar Gupta <202217B3005@wilp.bits-pilani.ac.in>
2025-05-15 11:33:27 +0530

    Update udid_status_script.py

--- udid_status_script.py
+++ udid_status_script.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 import requests
 
 # === CONFIGURATION ===
